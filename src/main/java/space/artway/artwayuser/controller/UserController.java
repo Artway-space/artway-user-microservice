0d3c9659bf60adecac3163c80b24572471378e52
andrey <andrewyelmanov@gmail.com>
2021-05-27 00:01:43 +0300

    Eureka connect

--- src/main/java/space/artway/artwayuser/controller/UserController.java
+++ src/main/java/space/artway/artwayuser/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @RestController
 @RequestMapping("api/user")
 public class UserController {
