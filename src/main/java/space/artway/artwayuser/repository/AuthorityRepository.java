613fdb21be9b341e3698a7a067e957f5be8eb438
andrey <andrewyelmanov@gmail.com>
2021-06-14 21:57:58 +0300

    Fixed some bugs

--- src/main/java/space/artway/artwayuser/repository/AuthorityRepository.java
+++ src/main/java/space/artway/artwayuser/repository/AuthorityRepository.java
@@ -3,6 +3,6 @@ package space.artway.artwayuser.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import space.artway.artwayuser.domain.Authority;
 
-public interface AuthorityRepository extends JpaRepository<String, Authority> {
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
 
 }
